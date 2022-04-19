59bb58c775e276b4443bb7fcd4b4a33210f91f27
dstjr2434 <dstjr2434@gmail.com>
2022-04-19 17:35:25 +0900

    왕실

--- 왕실의 나이트.py
+++ 왕실의 나이트.py
@@ -2,12 +2,13 @@ input='a1'
 row=int(input[1])
 column=int(ord(input[0])) - int(ord('a'))+1
 
+x,y = list(input)
 
 print(row)
 print(column)
 
 steps=[(-2,-1),(-2,1),(2,-1),(2,-1),(-1,-2),(-1,2),(1,-2),(1,2)]
 
-x,y = list(input)
+
 
 
